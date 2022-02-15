cd1e0b41d76fac995b3d34f1f1af4d6f032bf3a6
moustafahawi <hawim@bzz.ch>
2022-02-15 23:18:59 +0100

    Created the needed classes SnakeJava, GameFrame and GamePanel.

---
+++ src/GameFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author Moustafa Sven Hawi
+ * @date 15.02.2022
+ * @version 1.0
+ * This code is from a YouTube video. The creator of the video is called Bro Code.
+ */
+public class GameFrame extends JFrame {
+    //Constructor
+    public GameFrame(){
+
+    }
+}
---
+++ src/SnakeJava.java
@@ -0,0 +1,11 @@
+/**
+ * @author Moustafa Sven Hawi
+ * @date 15.02.2022
+ * @version 1.0
+ * This code is from a YouTube video. The creator of the video is called Bro Code.
+ */
+public class SnakeJava {
+    public static void main(String[] args) {
+
+    }
+}
