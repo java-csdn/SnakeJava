a137bd16fb3b6758166bdce04125434504074b8b
moustafahawi <hawim@bzz.ch>
2022-02-16 09:55:39 +0100

    In GamePanel die Methode startGame und den Konstruktor fertig koodiert. Alle Variablen erstellt in GamePanel.

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,17 +13,50 @@ import java.util.Random;
  * Dieser Code ist von einem YouTube Video, der Ersteller des Videos ist Bro Code.
  */
 public class GamePanel extends JPanel implements ActionListener {
-    //Constructor
+    //Grösse des Frames festlegen.
+    public static final int SCREEN_WIDTH = 600;
+    public static final int SCREEN_HIGHT = 600;
+    //Grösse der Objekte im Spiel festlegen.
+    public static final int UNIT_SIZE = 25;
+    //Berechnet wie viel Objekte im Frame platz haben.
+    public static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HIGHT)/UNIT_SIZE;
+    public static final int DELAY = 75;
+    //Diese Arrays halten die x und y Koordinaten der Schlange.
+    public int x[] = new int[GAME_UNITS];
+    public int y[] = new int[GAME_UNITS];
+    //Mit wie vielen Körperteilen man anfängt.
+    int bodyParts = 6;
+    int applesEaten;
+    //Wo der Apfel landet auf der x Koordinate. Wird random gewählt.
+    int appleX;
+    //Wo der Apfel landet auf der y Koordinate. Wird random gewählt.
+    int appleY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+    //Konstruktor
     public GamePanel(){
-
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        startGame();
     }
     public void startGame(){
-
+        newApple();
+        running = true;
+        timer = new Timer(DELAY, this);
+        timer.start();
     }
     public void paintComponent(Graphics g){
-
+        super.paintComponent(g);
     }
     public void draw(Graphics g){
+
+    }
+    public void newApple(){
 
     }
     public void move(){
